bd21254bb31622fd801a24d5a63d737f37843413
limboaz <limboaz@outlook.com>
2019-03-07 11:50:37 -0500

    big bowl of cereal

--- routes/speak.js
+++ routes/speak.js
@@ -4,17 +4,18 @@ var router = express.Router();
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(err, conn) {
-    conn.createChannel(function(err, ch) {
-        var ex = 'direct_logs';
-        var args = process.argv.slice(2);
-        var msg = args.slice(1).join(' ') || 'Hello World!';
-        var severity = (args.length > 0) ? args[0] : 'info';
+router.post('/', function (req, res) {
+    amqp.connect('amqp://localhost', function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            var ex = 'hw3';
+            var msg = req.body.msg;
+            var severity = req.body.key;
 
-        ch.assertExchange(ex, 'direct', {durable: false});
-        ch.publish(ex, severity, new Buffer(msg));
-        console.log(" [x] Sent %s: '%s'", severity, msg);
+            ch.assertExchange(ex, 'direct', {durable: false});
+            ch.publish(ex, severity, new Buffer(msg));
+            console.log(" [x] Sent %s: '%s'", severity, msg);
+        });
+
+        setTimeout(function() { conn.close(); process.exit(0) }, 500);
     });
-
-    setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
